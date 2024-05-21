8338da49971a2f907223e952a00be935aadd63c8
Prayag <bipin@vrajsoft.com>
2024-05-21 12:33:58 +0530

    Add project files.

---
+++ CoreAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
